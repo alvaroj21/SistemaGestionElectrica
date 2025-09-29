2380bf4f22902844769cdf8e6c69fbb72b5fe0b8
Alvaro Pinto <nuggetantan@gmail.com>
2025-09-29 10:03:06 -0300

    models ejemplo
    Fixed class definition and field types for Clientes model.

--- sistemaGestion/models.py
+++ sistemaGestion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+#Definido modelo a modo de ejemplo para ir realizando los demas.
+class Clientes(models,Model):
+  numero_cliente = models.Charfield(max_length=50)
+  nombre = models.Charfield(max_length=50)
+  email = models.Charfield(max_length=50)
+  telefono = models.Charfield(max_length=15)
+  
+  
